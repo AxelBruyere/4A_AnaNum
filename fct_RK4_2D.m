9a8a0292f9cbc62c50d5a2c7174e84f7b7dbc574
Axel Bruyere <ax.bru99@gmail.com>
2021-03-28 20:42:28 +0200

    ajout de RK4-4D et double pendule problème

--- fct_RK4_2D.m
+++ fct_RK4_2D.m
@@ -23,6 +23,8 @@ for k = 1:length(t)-1
     
     x(k+1) = x(k) + (pas/6) * (kf1 + 2 * kf2 + 2 * kf3 + kf4);
     y(k+1) = y(k) + (pas/6) * (kg1 + 2 * kg2 + 2 * kg3 + kg4);
+    u(k+1) = u(k) + (pas/6) * (kh1 + 2 * kh2 + 2 * kh3 + kh4);
+    v(k+1) = v(k) + (pas/6) * (ki1 + 2 * ki2 + 2 * ki3 + ki4);
     
 end
   
